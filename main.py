7e4f6077fb8720a95e810e3d65a132aa8bb1873c
bipin-Pariyar2003 <163013814+bipin-Pariyar2003@users.noreply.github.com>
2025-05-19 10:26:48 +0545

    portfolio-qr

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ import qrcode
 img = qrcode.make("https://nepali-date-picker-alpha.vercel.app/")
 type(img)
 img.save("date-picker-qr.jpg")
+
+
+img1 = qrcode.make("https://bipinpariyar2003.pythonanywhere.com/")
+type(img1)
+img1.save("portfolio-qr.jpg")
